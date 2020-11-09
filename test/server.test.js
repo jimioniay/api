0e54315f105c3f7503f408a9507aea77d50d12e5
Ayomide Jimi-Oni <ayomidejimi-oni@Ayomides-MacBook-Pro.local>
2020-11-09 01:27:56 -0500

    add travis script

--- test/server.test.js
+++ test/server.test.js
@@ -5,5 +5,8 @@ describe('Test Server.js', () => {
     it('should return 200', async () => {
       expect(200).to.equal(200);
     });
+    it('should return 400', async () => {
+      expect(400).to.equal(400);
+    });
   });
 });
